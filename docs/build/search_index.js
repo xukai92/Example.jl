9087449340a1c5cdb1bf3041fdcda180f5c6c8de
Kai Xu <contact@xuk.ai>
2022-02-22 21:04:40 +0000

    track build

---
+++ docs/build/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Example.jl","page":"Example.jl","title":"Example.jl","text":"","category":"section"},{"location":"","page":"Example.jl","title":"Example.jl","text":"Documentation for Example.jl","category":"page"}]
+}
